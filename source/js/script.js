9bf6759829697f0537b3e7c7b5b928bcac5ff7e0
Roman Shelepov <shelepov-rmn@mail.ru>
2024-03-02 04:23:17 +0300

    Разметка и первичная стилизация окна Login

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,23 @@
 "use strict";
 
+// Навигация в шапке сайта
 const navToggle = document.querySelector(".page-header__burger-btn");
 const navList = document.querySelector(".page-nav__list");
 
 navToggle.addEventListener("click", function () {
     navToggle.classList.toggle("page-header__burger-btn--opened");
     navList.classList.toggle("page-nav__list--opened");
+});
+
+// Окно Log in
+const navLoginBtn = document.querySelector(".user-nav__login");
+const modalWindow = document.querySelector(".modal-container");
+const modalCloseBtn = document.querySelector(".modal__close-btn");
+
+navLoginBtn.addEventListener("click", function () {
+    modalWindow.classList.toggle("modal--opened");
+});
+
+modalCloseBtn.addEventListener("click", function () {
+    modalWindow.classList.toggle("modal--opened");
 });
