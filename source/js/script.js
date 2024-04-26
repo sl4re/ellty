4c7e43495107b7f28571beaac07d58c92dc3f0ba
Roman Shelepov <shelepov-rmn@mail.ru>
2024-04-26 21:12:48 +0300

    Сделал основную стилизацию всей страницы Create Design. Сделал JS-логику для отображения фильтров

--- source/js/script.js
+++ source/js/script.js
@@ -36,3 +36,71 @@ navSignUpBtn.addEventListener("click", function () {
 signupCloseBtn.addEventListener("click", function () {
     modalSignUp.classList.toggle("modal--opened");
 });
+
+// Фильтры на странице Create Design
+
+const showPopular = document.querySelector(".show-popular");
+const showMedia = document.querySelector(".show-media");
+const showMarketing = document.querySelector(".show-marketing");
+const showAdvertising = document.querySelector(".show-advertising");
+const showBranding = document.querySelector(".show-branding");
+const showOffice = document.querySelector(".show-office");
+const showMore = document.querySelector(".show-more");
+
+const filtersPopular = document.querySelector(".popular");
+const filtersMedia = document.querySelector(".social-media");
+const filtersMarketing = document.querySelector(".marketing");
+const filtersAdvertising = document.querySelector(".advertising");
+const filtersBranding = document.querySelector(".branding");
+const filtersOffice = document.querySelector(".office");
+const filtersMore = document.querySelector(".more");
+
+showPopular.addEventListener("click", function () {
+    filtersPopular.classList.toggle("popular--opened");
+});
+
+showMedia.addEventListener("click", function () {
+    filtersMedia.classList.toggle("social-media--opened");
+});
+
+showMarketing.addEventListener("click", function () {
+    filtersMarketing.classList.toggle("marketing--opened");
+});
+
+showAdvertising.addEventListener("click", function () {
+    filtersAdvertising.classList.toggle("advertising--opened");
+});
+
+showBranding.addEventListener("click", function () {
+    filtersBranding.classList.toggle("branding--opened");
+});
+
+showOffice.addEventListener("click", function () {
+    filtersOffice.classList.toggle("office--opened");
+});
+
+showMore.addEventListener("click", function () {
+    filtersMore.classList.toggle("more--opened");
+});
+
+// Открытие фильтров
+
+const showFilters = document.querySelector(".design-filters__show-btn");
+const filtersWrapper = document.querySelector(".design-filters__inner");
+const filtersMobile = document.querySelector(".design-filters");
+const contentWrapper = document.querySelector(".design-content__inner");
+
+if (document.documentElement.clientWidth > 1024) {
+    showFilters.addEventListener("click", function () {
+        filtersWrapper.classList.toggle("filters--open");
+        contentWrapper.classList.toggle("design-content__inner--filters-closed");
+    });
+}
+
+// Открытие фильтров на мобилке
+if (document.documentElement.clientWidth <= 1024) {
+    showFilters.addEventListener("click", function () {
+        filtersMobile.classList.toggle("filters--open-mobile");
+        filtersWrapper.classList.remove("filters--open");
+    });
+}
